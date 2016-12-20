637ab6f6217d1ebc70dfe935456da57cac4de051
Phil Nash <philnash@gmail.com>
2016-12-20 10:41:48 +0000

    Removes noop after returning text from fetch

--- bot.js
+++ bot.js
@@ -6,8 +6,6 @@ var bot = botBuilder(function(message) {
   if (message.text.match(/quote/i)) {
     return fetch('http://api.forismatic.com/api/1.0/?method=getQuote&format=text&lang=en').then(function(res) {
       return res.text();
-    }).then(function(text) {
-      return text;
     });
   } else {
     return greeting.random();
